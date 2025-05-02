69c012d3b5ae69007380576ae66ee453ee2b074b
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-05-02 10:50:55 +0330

    feat: add panel structure

---
+++ src/app/[locale]/dashboard/components/Sidebar.tsx
@@ -0,0 +1,89 @@
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Avatar,
+  Typography,
+  Box,
+  Divider,
+  ListItemButton,
+} from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import MailIcon from '@mui/icons-material/Mail';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { DRAWER_MIN_WIDTH, DRAWER_WIDTH } from '@/constants/general';
+import { FC } from 'react';
+
+const drawerWidth = DRAWER_WIDTH;
+const miniWidth = DRAWER_MIN_WIDTH;
+
+interface SidebarProps {
+  collapsed: boolean;
+}
+
+const Sidebar: FC<SidebarProps> = ({ collapsed }) => {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: collapsed ? miniWidth : drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: collapsed ? miniWidth : drawerWidth,
+          boxSizing: 'border-box',
+          transition: 'width 0.3s',
+          bgcolor: '#ebebeb', // TODO
+        },
+      }}
+    >
+      <Box sx={{ p: 2, height: '100%' }}>
+        <Box display="flex" flexDirection="column" alignItems="center" mt={7}>
+          <Avatar sx={{ width: 56, height: 56, mb: 1 }} />
+          {!collapsed && (
+            <>
+              <Typography variant="subtitle1">Bahar Keshavarz</Typography>
+              <Typography variant="caption">
+                bahar.keshavarzc@gmail.com
+              </Typography>
+            </>
+          )}
+        </Box>
+        <Divider />
+        <List>
+          {[
+            { text: 'Dashboard', icon: <DashboardIcon /> },
+            { text: 'Inbox', icon: <MailIcon /> },
+            { text: 'Contacts', icon: <ContactsIcon /> },
+            { text: 'Settings', icon: <SettingsIcon /> },
+          ].map(({ text, icon }) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: collapsed ? 'center' : 'flex-start',
+                  px: collapsed ? 1 : 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: collapsed ? 0 : 2,
+                    justifyContent: 'center',
+                  }}
+                >
+                  {icon}
+                </ListItemIcon>
+                {!collapsed && <ListItemText primary={text} />}
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,3 +1,4 @@
-export const DEFAULt_NO_IMAGE = "/assets/images/placeholders/no-image.jpg";
-export const HOME_CHAT_EXPERIENCE = "/assets/images/home/home-chat.png";
-export const drawerWidth = 260;
+export const DEFAULt_NO_IMAGE = '/assets/images/placeholders/no-image.jpg';
+export const HOME_CHAT_EXPERIENCE = '/assets/images/home/home-chat.png';
+export const DRAWER_WIDTH = 240;
+export const DRAWER_MIN_WIDTH = 72;
