3ec7ed5e0efe03ecef4ed646cd574223d8cfcd5e
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-05-03 14:19:59 +0330

    feat: add logout button

---
+++ src/app/[locale]/dashboard/components/SignOutButton.tsx
@@ -0,0 +1,30 @@
+import { useMutation } from '@tanstack/react-query';
+import auth from '@/lib/auth';
+import { useTranslations } from 'next-intl';
+import { ButtonWithLoading } from '@/components/ButtonWithLoading';
+import { Logout } from '@mui/icons-material';
+
+const SignOutButton = () => {
+  const t = useTranslations();
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: auth.logout,
+  });
+
+  const handleClickOnSignOut = async () => {
+    await mutateAsync();
+  };
+
+  return (
+    <ButtonWithLoading
+      isLoading={isPending}
+      onClick={handleClickOnSignOut}
+      variant="outlined"
+      color="error"
+      startIcon={<Logout />}
+    >
+      {t('common:buttons.signOut')}
+    </ButtonWithLoading>
+  );
+};
+
+export default SignOutButton;
