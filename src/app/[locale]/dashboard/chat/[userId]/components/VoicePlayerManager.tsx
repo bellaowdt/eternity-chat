5db8087834b68c03ac3edbb58ad4544e856e2bb2
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-06-12 19:30:36 +0330

    refactor: make the voice component as a hook

--- src/app/[locale]/dashboard/chat/[userId]/components/VoicePlayerManager.tsx
+++ src/app/[locale]/dashboard/chat/[userId]/components/VoicePlayerManager.tsx
@@ -1,36 +1,19 @@
 import VoicePlayer from '@/components/VoicePlayer';
-import { textToSpeech } from '@/services/chat';
-import { CircularProgress, IconButton, Tooltip } from '@mui/material';
-import { useMutation } from '@tanstack/react-query';
-import { useTranslations } from 'next-intl';
-import { FC, useState } from 'react';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import { CircularProgress, IconButton, Tooltip } from '@mui/material';
+import { useTranslations } from 'next-intl';
+import { FC } from 'react';
+import useAudioPlayer from '../hooks/useAudioPlayer';
 
 interface VoicePlayerManagerProps {
   value?: string;
 }
 const VoicePlayerManager: FC<VoicePlayerManagerProps> = ({ value }) => {
   const t = useTranslations();
-  const [audioUrl, setAudioUrl] = useState<string | null>(null);
-
-  const { mutate, isPending } = useMutation({
-    mutationFn: async () => {
-      const { data } = await textToSpeech({
-        payload: {
-          text: value!,
-        },
-      });
-
-      // Convert arraybuffer to Blob and URL
-      const blob = new Blob([data as ArrayBuffer], { type: 'audio/mpeg' });
-      const url = URL.createObjectURL(blob);
-      setAudioUrl(url);
-    },
+  const { onTexttoSpeech, audioUrl, isPending } = useAudioPlayer({
+    text: value || '',
   });
 
-  const onTexttoSpeech = () => {
-    mutate();
-  };
   return (
     <>
       <Tooltip title={t('pages.chat.toolbarIcons.readAloud')} arrow>
