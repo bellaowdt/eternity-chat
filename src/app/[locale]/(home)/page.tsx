6e6f491a3a6fcbb5bb4cdb351a5a6c30c2c94eb0
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2025-07-07 18:19:35 +0330

    fix: redirect home page to onboarding

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -6,17 +6,21 @@ import { Container } from '@mui/material';
 import { Header } from '@/components/Header';
 import { Navbar } from '@/components/Navbar';
 import useResponsiveContainer from '@/hooks/useResponsiveContainer';
+import { DEFAULT_ONBOARDING_PATH } from '@/constants/routes';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  router.push(DEFAULT_ONBOARDING_PATH);
   const containerMaxWidth = useResponsiveContainer();
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Header />
       <Container maxWidth={containerMaxWidth}>
         <ChatExperience />
         <ChooseUs />
-      </Container>
+      </Container> */}
     </>
   );
 }
